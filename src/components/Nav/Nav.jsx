c0008f0bf046382250fed15532abf9bc1205e391
baitemir <baitemir1706@gmail.com>
2023-12-03 17:54:17 +0600

    navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,47 @@
-import NavItem from "./NavItem/NavItem";
 import styles from "./Nav.module.scss"
+import logo from '../../assets/logo.png'
+import fb from '../../assets/fb.svg'
+import ig from '../../assets/ig.svg'
+import tg from '../../assets/tg.svg'
+import wa from '../../assets/wa.svg'
+import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className={styles.Nav}>
+      <div>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <h1>Организация Объединенных Кыргызских Оценщиков</h1>
+        <div className={styles.social}>
+          <a href="#"><img src={fb} alt="fb" /></a>
+          <a href="#"><img src={ig} alt="ig" /></a>
+          <a href="#"><img src={tg} alt="tg" /></a>
+          <a href="#"><img src={wa} alt="wa" /></a>
+        </div>
+      </div>
       <ul>
         <li>
-          <NavItem> home</NavItem>
+          <NavLink to={'/'}>Главная страница</NavLink>
         </li>
         <li>
-          <NavItem> home</NavItem>
+          <NavLink  to={'/about'}>О нас</NavLink>
         </li>
         <li>
-          <NavItem> home</NavItem>
+          <NavLink to={'/members'}>Члены объединения </NavLink>
+        </li>
+        <li>
+          <NavLink>Новости</NavLink>
+        </li>
+        <li>
+          <NavLink>Реестр</NavLink>
+        </li>
+        <li>
+          <NavLink>НПА</NavLink>
+        </li>
+        <li>
+          <NavLink>Органы управления</NavLink>
+        </li>
+        <li>
+          <NavLink>Курсы</NavLink>
         </li>
       </ul>
     </nav>
--- src/components/Nav/NavItem/NavItem.jsx
+++
@@ -1,8 +0,0 @@
-import { NavLink } from "react-router-dom";
-
-const NavItem = ({ children }) => {
-console.log(children)
-  return <NavLink to={children}>{children}</NavLink>;
-};
-
-export default NavItem;
