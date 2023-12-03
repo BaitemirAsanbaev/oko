287d851a88edc0c984157d81a88de52b25efe4dc
baitemir <baitemir1706@gmail.com>
2023-12-03 22:13:17 +0600

    members

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Members from "./pages/Members/Members";
 
 function App() {
   return (
     <>
       <Layout>
         <Routes>
-        <Route index path="/" element={<Home />} />
+          <Route index path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          
+          <Route path="/members/:id" element={<Members />} />
         </Routes>
       </Layout>
     </>
