1fda92a4bc67af80bb463da7a344c1f255583bdd
baitemir <baitemir1706@gmail.com>
2023-12-08 19:48:27 +0600

    npa

---
+++ src/pages/NPA/NPA.jsx
@@ -0,0 +1,82 @@
+
+export default function NPA() {
+  return (
+    <div className="page">
+      <h1>НПА</h1>
+      <ul>
+        <li>
+          <a href="#low1">Закон#1</a>
+        </li>
+        <li>
+          <a href="low2">Закон#2</a>
+        </li>
+        <li>
+          <a href="#low3">Закон#3</a>
+        </li>
+      </ul>
+      <p id="low1">
+        <b>low1</b> ipsum dolor sit amet consectetur, adipisicing elit.
+        Neque vero corrupti odit? Accusamus quisquam eaque inventore ab
+        consectetur, repellendus facilis in, fugiat sed, cum dolor officia porro
+        saepe eveniet corporis. Asperiores sequi omnis id sapiente hic earum.
+        Iusto rerum reiciendis libero at expedita, quisquam dolor quia accusamus
+        accusantium modi id illum voluptates magni, qui, eius quas possimus.
+        Natus, maxime quos. Tempora facere minus laudantium aut sit nobis optio
+        quidem corporis maxime quae. Aperiam harum mollitia necessitatibus alias
+        sapiente natus porro quas esse, debitis doloribus iste commodi odit.
+        Eveniet, exercitationem minima. Quasi libero qui, praesentium distinctio
+        blanditiis animi perspiciatis molestias, enim ex officiis rerum ipsam
+        optio quaerat obcaecati? Quod neque voluptas omnis facere aperiam odit a
+        dolor, accusantium, error quas cumque. Enim rem vero in? A ipsum, non
+        odit eos exercitationem accusantium atque ullam earum ipsa at facere,
+        ipsam necessitatibus rerum officiis tenetur minima placeat, assumenda
+        mollitia qui aliquam fugiat quo. Debitis facilis asperiores est quas
+        eaque! In reiciendis qui ipsa unde! Eligendi iure quo iste deleniti
+        sapiente, beatae dolore. Esse fuga excepturi quis distinctio cupiditate
+        nemo veniam exercitationem quas deserunt.
+      </p>
+      <p id="low2">
+        <b>low2</b> ipsum dolor sit amet consectetur, adipisicing
+        elit. Neque vero corrupti odit? Accusamus quisquam eaque inventore ab
+        consectetur, repellendus facilis in, fugiat sed, cum dolor officia porro
+        saepe eveniet corporis. Asperiores sequi omnis id sapiente hic earum.
+        Iusto rerum reiciendis libero at expedita, quisquam dolor quia accusamus
+        accusantium modi id illum voluptates magni, qui, eius quas possimus.
+        Natus, maxime quos. Tempora facere minus laudantium aut sit nobis optio
+        quidem corporis maxime quae. Aperiam harum mollitia necessitatibus alias
+        sapiente natus porro quas esse, debitis doloribus iste commodi odit.
+        Eveniet, exercitationem minima. Quasi libero qui, praesentium distinctio
+        blanditiis animi perspiciatis molestias, enim ex officiis rerum ipsam
+        optio quaerat obcaecati? Quod neque voluptas omnis facere aperiam odit a
+        dolor, accusantium, error quas cumque. Enim rem vero in? A ipsum, non
+        odit eos exercitationem accusantium atque ullam earum ipsa at facere,
+        ipsam necessitatibus rerum officiis tenetur minima placeat, assumenda
+        mollitia qui aliquam fugiat quo. Debitis facilis asperiores est quas
+        eaque! In reiciendis qui ipsa unde! Eligendi iure quo iste deleniti
+        sapiente, beatae dolore. Esse fuga excepturi quis distinctio cupiditate
+        nemo veniam exercitationem quas deserunt.
+      </p>
+      <p id="low3">
+        <b>low3</b> ipsum dolor sit amet consectetur, adipisicing
+        elit. Neque vero corrupti odit? Accusamus quisquam eaque inventore ab
+        consectetur, repellendus facilis in, fugiat sed, cum dolor officia porro
+        saepe eveniet corporis. Asperiores sequi omnis id sapiente hic earum.
+        Iusto rerum reiciendis libero at expedita, quisquam dolor quia accusamus
+        accusantium modi id illum voluptates magni, qui, eius quas possimus.
+        Natus, maxime quos. Tempora facere minus laudantium aut sit nobis optio
+        quidem corporis maxime quae. Aperiam harum mollitia necessitatibus alias
+        sapiente natus porro quas esse, debitis doloribus iste commodi odit.
+        Eveniet, exercitationem minima. Quasi libero qui, praesentium distinctio
+        blanditiis animi perspiciatis molestias, enim ex officiis rerum ipsam
+        optio quaerat obcaecati? Quod neque voluptas omnis facere aperiam odit a
+        dolor, accusantium, error quas cumque. Enim rem vero in? A ipsum, non
+        odit eos exercitationem accusantium atque ullam earum ipsa at facere,
+        ipsam necessitatibus rerum officiis tenetur minima placeat, assumenda
+        mollitia qui aliquam fugiat quo. Debitis facilis asperiores est quas
+        eaque! In reiciendis qui ipsa unde! Eligendi iure quo iste deleniti
+        sapiente, beatae dolore. Esse fuga excepturi quis distinctio cupiditate
+        nemo veniam exercitationem quas deserunt.
+      </p>
+    </div>
+  );
+}
